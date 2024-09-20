2424fae5e5340f57e88d6cc9e0ba498fc5340b7b
Bhuvanamadhan2018 <bhuvanamadhan19@gmail.com>
2024-09-20 13:13:22 +0530

    modified

--- currencyConvertetr/script.js
+++ currencyConvertetr/script.js
@@ -1,30 +1,76 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const convertButton = document.getElementById("convert");
+    convertButton.addEventListener("click", convertCurrency)
+    let currencyInfo;
+
+    function getListOfCurrencies() {
+        return fetch("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json").then(res => res.json())
+    }
 
 
 
-const exchangeRates = {
-  USD: 1,
-  INR: 74.83,
-  
-};
+    function buildCurrencyOptions() {
 
-document.getElementById('convertBtn').addEventListener('click', function() {
-  const amount = parseFloat(document.getElementById('amount').value);
-  const fromCurrency = document.getElementById('fromCurrency').value;
-  const toCurrency = document.getElementById('toCurrency').value;
+        let listOfCurrencies;
+        getListOfCurrencies().then(result => {
+            listOfCurrencies = result
+            console.log(listOfCurrencies);
+            let fromCurrency = document.getElementById("fromCurrency");
+            let toCurrency = document.getElementById("toCurrency");
 
-  if (isNaN(amount) || !exchangeRates[fromCurrency] || !exchangeRates[toCurrency]) {
-    alert('Invalid input');
-    return;
-  }
+            for (let key in listOfCurrencies) {
+                let fromCurrencyOption = document.createElement("option");
+                fromCurrencyOption.id = `${key}_from`;
+                fromCurrencyOption.value = key;
+                fromCurrencyOption.textContent = listOfCurrencies[key];
+                fromCurrency.appendChild(fromCurrencyOption);
+                let toCurrencyOption = document.createElement("option");
+                toCurrencyOption.id = `${key}_to`;
+                toCurrencyOption.value = key;
+                toCurrencyOption.textContent = listOfCurrencies[key];
+                toCurrency.appendChild(toCurrencyOption);
+            }
 
-  const result = convertCurrency(amount, fromCurrency, toCurrency);
-  document.getElementById('result').innerText = `${amount} ${fromCurrency} is equal to ${result} ${toCurrency}`;
-});
+        });
+        console.log(listOfCurrencies);
 
-function convertCurrency(amount, fromCurrency, toCurrency) {
-  if (fromCurrency !== "USD") {
-    amount = amount / exchangeRates[fromCurrency];
-  }
-  const convertedAmount = amount * exchangeRates[toCurrency];
-  return convertedAmount.toFixed(2);
-}
+
+
+
+    }
+
+    function loadCurrencyValues() {
+        fetch("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json")
+            .then(res => res.json())
+            .then(data => currencyInfo = data);
+    }
+
+    function convertCurrency() {
+        let fromCurrency = document.getElementById("fromCurrency");
+        let toCurrency = document.getElementById("toCurrency");
+
+        const selectedFromCurrency = fromCurrency.value;
+        const selectedToCurrency = toCurrency.value;
+
+        if (selectedFromCurrency && selectedToCurrency) {
+            let { eur } = currencyInfo;
+            let convertedAmount = eur[selectedToCurrency] / eur[selectedFromCurrency];
+            let result = document.getElementById("result");
+            let amountToConvert = document.getElementById("amount").valueAsNumber;
+            result.value = (amountToConvert * convertedAmount).toFixed(2);
+            console.log(currencyInfo, selectedFromCurrency, selectedToCurrency);
+
+        }
+
+
+    }
+    convertButton.addEventListener("click", () => {
+
+    })
+
+    loadCurrencyValues();
+    buildCurrencyOptions();
+
+})
